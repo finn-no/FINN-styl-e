f3ae56067625e60bb1f6e02f931f5cd79e2aea5e
Sveinung Rosaker <sveinung.rosaker@gmail.com>
2011-10-23 10:29:46 +0200

    Wip

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-require('colors');
+require('colors'); // adds methods to the global String object
 
 GLOBAL.log = function() {
   var args = Array.prototype.slice.apply(arguments);
   console.log.apply(console, ["FINNSTYLE ->".green].concat(args));
 }
-
+console.log("\n================= STARTING ===================".yellow.bold);
+GLOBAL.TOP_DIR_NAME = __dirname;
 require('./lib/index');
+
+
