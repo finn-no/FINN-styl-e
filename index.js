2f13b579ba554b93623da7b16cc26cd71fa1baa8
Sveinung Rosaker <sveinung.rosaker@gmail.com>
2011-10-24 10:06:52 +0200

    Updated to support production config file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('colors'); // adds methods to the global String object
 
 GLOBAL.log = function() {
   var args = Array.prototype.slice.apply(arguments);
-  console.log.apply(console, ["FINNSTYLE ->".green].concat(args));
+  console.log.apply(console, ["[FINN-styl-e] ->".green].concat(args));
 }
 
 console.log("\n================= STARTING ===================".yellow.bold);
