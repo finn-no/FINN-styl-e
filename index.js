fdaccb0fd9e4a0df6b699f3bb44f77009f77eca2
Sveinung Rosaker <sveinung.rosaker@gmail.com>
2011-10-25 08:11:45 +0200

    Addding oocss styl

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 require('colors'); // adds methods to the global String object
-
-GLOBAL.log = function() {
-  var args = Array.prototype.slice.apply(arguments);
-  console.log.apply(console, ["[FINN-styl-e] ->".green].concat(args));
-}
-
 console.log("\n================= STARTING ===================".yellow.bold);
 GLOBAL.TOP_DIR_NAME = __dirname;
 require('./lib/index');
